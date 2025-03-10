a4f0c2016d94c9268a6523706c2730ca1b32c4f6
Brian Rios <riosbrian.ar@gmail.com>
2025-03-10 17:07:02 +0100

    feat: The i18n configuration, basic folder structure, and main shared components were developed

---
+++ src/app/songs/pages/songs-page/songs-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-songs-page',
+  imports: [],
+  templateUrl: './songs-page.component.html',
+  styleUrl: './songs-page.component.css'
+})
+export class SongsPageComponent {
+
+}
